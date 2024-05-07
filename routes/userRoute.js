297d60b41f478b4012fa6dae251ae2f0ad4e1d8e
sabtech <sabstechgh@gmail.com>
2024-05-06 20:57:16 -0400

    progress

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -10,17 +10,6 @@ const axios = require('axios');
 
 
 
-const connection = mysql.createConnection({
-
-    'host': 'localhost',
-
-    'user': 'sabtechhub_dnmtc2_user',
-
-    'password': '[u%g#YQ&hD#(',
-
-    'database': 'sabtechhub_dnmtc_2'
-
-});
 
 
 // const connection = mysql.createConnection({
@@ -269,7 +258,7 @@ async function momoPromptWeb(destination, amount, client_ref, network,email) {
 
         foreignID: client_ref,
 
-        'callbackUrl' :'https://app.smartvotegh.com/status'
+        
 
     };
 
@@ -281,11 +270,7 @@ async function momoPromptWeb(destination, amount, client_ref, network,email) {
 
             headers: {
 
-                'clientid': 'Qys10304',
-
-                'Content-Type': 'application/json',
-
-                'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjoiIiwiaWF0IjoxNjY1MDkzMjQxfQ.pRFO8MCG06NkeXJ9mCefTi90yMKLu8tdiEUvLlxiWl4WkgfygXwCdrqs5aywDOwAL_7QQXv5Mul7_ASBZS25g-klBUBUMi6yw4HPw6SBLv-Q0ktoSCjIRWlL3N9-V-bHhndn1qAD2wU4HShhtFhT8gxoaNj5IvWv_upEdRMky2f6Ky6P_rZuv5wVMn_9QjfDeGuRnPcvrbwXO0k_1kGGhYBjozeOyWARhUUjwIAjIBH5u1SQATSpFEs20p308NeUeumiluaMve8MsVORP_bAVDkKM2ZMVvH609AUDrVmns-sFqoiwx9tl-nztfq8n_VJyaVmJQ3uJDSDQ1LKFWT3kicyJBl-Cg4QzFt0DggEu0WyWPWp311CBohLxp6A58bSuNkg5wwCP_Rfa_0Vh2PsJvZpYiLoGhTU1YnjCcspRiBAC6ijAcMfmdDbwRWvzSUzFmjXFFlJqxImbvetPI_kqunNQGHFVEfe9DgDWZAWfneReW4rJ5xPivEe28J0qJteDcqDgu9sET9n4oTmHmXfqlwpIz4ciPxhPdx2fWFAUomE0MSrG_n_wnHTlCqHtvc-xDJOSiSiwKkwGp7XfsgmPwtTb9DY0OwSKdgwY8s0ZJEUkUQ6TG66emZJPdxL4uH7g_fN2gJJuhUKBrIHiFeHjSs_gz5yID-7ewBlzOxq1X0'
+    
 
             }
 
